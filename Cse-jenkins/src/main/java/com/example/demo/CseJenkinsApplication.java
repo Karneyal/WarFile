847831d1ba9297ebf49d62686307386c527556a3
rockk <rockk@karneyal.lan>
2025-09-14 23:25:25 +0530

    this warfile.

---
+++ Cse-jenkins/src/main/java/com/example/demo/CseJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseJenkinsApplication.class, args);
+	}
+
+}
